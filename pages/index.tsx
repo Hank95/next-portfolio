52de9d9de808f902126bbfddf944fd2c5cf93c03
Henry <hhpendleton@gmail.com>
2022-04-07 09:12:49 -0400

    stying changes and added footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import NavBar from "../components/navBar";
 import Portfolio from "../components/portfolio";
 import Earth from "../components/earth";
 import Bio from "../components/bio";
+import Footer from "../components/footer";
 
 const Home: NextPage = () => {
   return (
@@ -38,18 +39,7 @@ const Home: NextPage = () => {
       </main>
       <Portfolio />
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <Footer />
     </div>
   );
 };
