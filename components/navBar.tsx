8b53b9ea97edb578806ee1bf51289ad93ada1d55
Hank95 <hhpendleton@gmail.com>
2022-04-18 11:07:49 -0400

    nav bar changes

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 // Navigation bar component with links to resumes and contact page
 
 const NavBar = () => {
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 750);
+
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 750);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  });
   return (
     <div className="grid grid-cols-2 items-center m-2 z-50 bg-slate-800 rounded-full ">
       {/* my logo here */}
@@ -16,6 +27,8 @@ const NavBar = () => {
           />
         </a>
       </Link>
+
+{ isDesktop ? 
       <div className=" flex flex-row text-white justify-end mx-20items-center">
         <div className="mx-10 p-4 rounded-full hover:text-amber-300 hover:bg-slate-500 transition duration-500 ease-in-out text-3xl ">
           <Link href="/">Home</Link>
@@ -27,6 +40,8 @@ const NavBar = () => {
           <Link href="/contact">Contact</Link>
         </div>
       </div>
+
+: <div> navigation bar </div>}
     </div>
   );
 };
